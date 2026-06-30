02573e7a35498f9282c9328779b0c4524b8d0e7d
jie44936-coder <jie44936@gmail.com>
2026-06-30 22:29:34 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
-BOT_TOKEN ="8969645448:AAE_JraPmRInECBqY6AOIVLZwlWV9-x1M2s"
+import os
 
+# 🔑 从 Render 环境变量读取 Token
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# 👮 管理员ID
 ADMIN_IDS = [8628525434]
 
+# ⚙️ 基础设置
 ENABLE_AUTO_DELETE = True
